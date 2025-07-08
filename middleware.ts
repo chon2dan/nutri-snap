1aa4762192b54e499ab1c2de9328682d32e13aee
hcPark <zipcodebaby@gmail.com>
2025-07-08 11:07:19 +0900

    프로펠러 애즈 js 삽입 미들웨어 수정

--- middleware.ts
+++ middleware.ts
@@ -12,6 +12,6 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico|ads.txt|robots.txt).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|ads.txt|robots.txt|sw.js).*)",
   ],
 };
