f13921ffd7b41577bd1751f4ea95992021348eab
hcPark <zipcodebaby@gmail.com>
2026-04-13 17:50:21 +0900

    feat: configure headers to serve app-ads.txt as plain text

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/app-ads.txt",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "text/plain; charset=utf-8",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
