a730cb31e21d0749b870c0515289f92b28ee1aba
hcPark <zipcodebaby@gmail.com>
2026-04-13 17:21:28 +0900

    구글애드몹 미들웨어 제어

--- middleware.ts
+++ middleware.ts
@@ -12,6 +12,6 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico|ads.txt|robots.txt|sw.js).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|app-ads.txt|ads.txt|robots.txt|sw.js).*)",
   ],
 };
